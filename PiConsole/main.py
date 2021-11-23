af999e297a60ca218b2e55caee78c3d68487f5e3
GCampbell2001 <GCampbell@student.neumont.edu>
2021-11-22 22:41:09 -0700

    Character Selection Code

--- PiConsole/main.py
+++ PiConsole/main.py
@@ -9,100 +9,100 @@ import os
 import keyboard
 
 
-def play_message(msg):
-    # print(msg)
-    encoded_msg = msg[0]
-    message_byte = base64.b64decode(encoded_msg)
-    m=[]
-    for i in message_byte:
-         m.append(i)
-    binary_format = bytearray(m)
-    f = open("testing.wav", 'w+b')
-    f.write(binary_format)
-    f.close
-    sound = sa.WaveObject.from_wave_file("testing.wav")
-    play_obj = sound.play()
-    play_obj.wait_done()
-    # print("This shouldn't print until after file is done playing")
-    os.remove("testing.wav")
-    # play(sound)
-
-
-def input_with_default(input_text, default_value):
-    value = input(input_text.format(default_value))
-    return default_value if value is None or value.strip() == "" else value
-
-
-server_url = input_with_default('Enter your server url(default: {0}): ', "wss://localhost:5001/chatHub")
-username = input_with_default('Enter your username (default: {0}): ', "mandrewcito")
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-hub_connection = HubConnectionBuilder()\
-    .with_url(server_url, options={"verify_ssl": False}) \
-    .configure_logging(logging.DEBUG, socket_trace=True, handler=handler) \
-    .with_automatic_reconnect({
-            "type": "interval",
-            "keep_alive_interval": 10,
-            "intervals": [1, 3, 5, 6, 7, 87, 3]
-        }).build()
-
-hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
-hub_connection.on_close(lambda: print("connection closed"))
-
-
-hub_connection.start()
-
-hub_connection.on("RecieveAudio", play_message)
-#hub_connection.on("ReceiveMessage", print)
-
-
-
-
-message = None
-
-# Do login
-
-while message != "exit()":
-    message = input(">> ")
-    if message is not None and message != "" and message != "exit()":
-        hub_connection.send("SendAudio", [])
-        print("I'M SENDING HERE")
-        print("I'M SENDING HERE")
-        print("I'M SENDING HERE")
-    else:
-        try:
-            # os.remove("testing.wav")
-            print("python is angy and this fixes it. lil *****")
-        finally:
-            print("line72")
-
+# def play_message(msg):
+#     # print(msg)
+#     encoded_msg = msg[0]
+#     message_byte = base64.b64decode(encoded_msg)
+#     m=[]
+#     for i in message_byte:
+#          m.append(i)
+#     binary_format = bytearray(m)
+#     f = open("testing.wav", 'w+b')
+#     f.write(binary_format)
+#     f.close
+#     sound = sa.WaveObject.from_wave_file("testing.wav")
+#     play_obj = sound.play()
+#     play_obj.wait_done()
+#     # print("This shouldn't print until after file is done playing")
+#     os.remove("testing.wav")
+#     # play(sound)
+#
+#
+# def input_with_default(input_text, default_value):
+#     value = input(input_text.format(default_value))
+#     return default_value if value is None or value.strip() == "" else value
+#
+#
+# server_url = input_with_default('Enter your server url(default: {0}): ', "wss://localhost:5001/chatHub")
+# username = input_with_default('Enter your username (default: {0}): ', "mandrewcito")
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.DEBUG)
+# hub_connection = HubConnectionBuilder()\
+#     .with_url(server_url, options={"verify_ssl": False}) \
+#     .configure_logging(logging.DEBUG, socket_trace=True, handler=handler) \
+#     .with_automatic_reconnect({
+#             "type": "interval",
+#             "keep_alive_interval": 10,
+#             "intervals": [1, 3, 5, 6, 7, 87, 3]
+#         }).build()
+#
+# hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
+# hub_connection.on_close(lambda: print("connection closed"))
+#
+#
+# hub_connection.start()
+#
+# hub_connection.on("RecieveAudio", play_message)
+# #hub_connection.on("ReceiveMessage", print)
+#
+#
+#
+#
+# message = None
+#
+# # Do login
+#
 # while message != "exit()":
 #     message = input(">> ")
 #     if message is not None and message != "" and message != "exit()":
-#         hub_connection.send("SendMessage", [username, message])
+#         hub_connection.send("SendAudio", [])
 #         print("I'M SENDING HERE")
-
-
-hub_connection.stop()
+#         print("I'M SENDING HERE")
+#         print("I'M SENDING HERE")
+#     else:
+#         try:
+#             # os.remove("testing.wav")
+#             print("python is angy and this fixes it. lil *****")
+#         finally:
+#             print("line72")
+#
+# # while message != "exit()":
+# #     message = input(">> ")
+# #     if message is not None and message != "" and message != "exit()":
+# #         hub_connection.send("SendMessage", [username, message])
+# #         print("I'M SENDING HERE")
+#
+#
+# hub_connection.stop()
 
 def play_game():
-    server_url = "wss://localhost:5001/chatHub"
-    # username = input_with_default('Enter your username (default: {0}): ', "mandrewcito")
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    hub_connection = HubConnectionBuilder() \
-        .with_url(server_url, options={"verify_ssl": False}) \
-        .configure_logging(logging.DEBUG, socket_trace=True, handler=handler) \
-        .with_automatic_reconnect({
-        "type": "interval",
-        "keep_alive_interval": 10,
-        "intervals": [1, 3, 5, 6, 7, 87, 3]
-    }).build()
-
-    hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
-    hub_connection.on_close(lambda: print("connection closed"))
-
-    hub_connection.start();
+    # server_url = "wss://localhost:5001/chatHub"
+    # # username = input_with_default('Enter your username (default: {0}): ', "mandrewcito")
+    # handler = logging.StreamHandler()
+    # handler.setLevel(logging.DEBUG)
+    # hub_connection = HubConnectionBuilder() \
+    #     .with_url(server_url, options={"verify_ssl": False}) \
+    #     .configure_logging(logging.DEBUG, socket_trace=True, handler=handler) \
+    #     .with_automatic_reconnect({
+    #     "type": "interval",
+    #     "keep_alive_interval": 10,
+    #     "intervals": [1, 3, 5, 6, 7, 87, 3]
+    # }).build()
+    #
+    # hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
+    # hub_connection.on_close(lambda: print("connection closed"))
+    #
+    # hub_connection.start();
 
     #TODO:          play intro audio.
     #TODO:          play character Intro Dialogues
@@ -111,10 +111,11 @@ def play_game():
     #TODO:          Read UserInput KeyPress and make calls to methods based of this.
 
     #skipping intro and going right into Intro Dialogues
-    need_to_make_choice = true
+    need_to_make_choice = True
     while need_to_make_choice:
         current_choice = 1
-        Determine_Character_Choice(current_choice)
+        choice = Determine_Character_Choice()
+        print(choice)
         # sound_reg = sa.WaveObject.from_wave_file("ReginaldIntro.wav")
         # sound_bel = sa.WaveObject.from_wave_file("BelladonnaIntro.wav")
         # sound_nodge = sa.WaveObject.from_wave_file("NodgeIntro.wav")
@@ -131,19 +132,63 @@ def play_game():
     hub_connection.stop();
 
 
-def Determine_Character_Choice(current_choice):
+def Determine_Character_Choice():
     sound_reg = sa.WaveObject.from_wave_file("ReginaldIntro.wav")
-    sound_bel = sa.WaveObject.from_wave_file("BelladonnaIntro.wav")
-    sound_nodge = sa.WaveObject.from_wave_file("NodgeIntro.wav")
+    play_reg = sound_reg
+    return reginald_choice(play_reg)
+
+
+def reginald_choice(reg):
+    #this is choice 1
+    key_not_pressed = True
+    try:
+        reg.stop()
+    except:
+        print()
+    sound_reg = sa.WaveObject.from_wave_file("ReginaldIntro.wav")
+    print("HitReggie")
     play_reg = sound_reg.play()
-    if (keyboard.is_pressed('down')):
-        if(current_choice == 1):
-            current_choice = 2
+    while key_not_pressed:
+        if keyboard.is_pressed('down'):
+            return belladonna_choice(play_reg)
+        if keyboard.is_pressed('enter'):
             play_reg.stop()
-            play_bell = sound_bel.play()
-        elif(current_choice == 2):
-            current_choice = 3
-            play_
+            return 1
 
 
+def belladonna_choice(reg):
+    #this is choice 2
+    key_not_pressed = True
+    try:
+        reg.stop()
+    except:
+        print()
+    sound_bell = sa.WaveObject.from_wave_file("BelladonnaIntro.wav")
+    print("htiBellodonna")
+    play_bell = sound_bell.play()
+    while key_not_pressed:
+        if keyboard.is_pressed('down'):
+            return nodge_choice(play_bell)
+        if keyboard.is_pressed('enter'):
+            play_bell.stop()
+            return 2
+
+def nodge_choice(reg):
+    #this is choice 3
+    key_not_pressed = True
+    try:
+        reg.stop()
+    except:
+        print()
+    sound_nodg = sa.WaveObject.from_wave_file("NodgeIntro.wav")
+    print("HitNodge")
+    play_nodg = sound_nodg.play()
+    if keyboard.is_pressed('down'):
+        return reginald_choice(play_nodg)
+    if keyboard.is_pressed('enter'):
+        play_nodg.stop()
+        return 3
+
+
+play_game()
 sys.exit(0)
